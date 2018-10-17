fca3c02c3273cd3822104d1592c7982fd3362bcd
Kristobal <junta.kristobal@gmail.com>
2018-10-17 23:19:41 +0300

    dynamic lettered columns; added html table view; refactored csv response

--- main.py
+++ main.py
@@ -30,37 +30,7 @@ def get_short_code(length=6):
             return short_id
 
 
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def show_form():
-    return render_template('index.html')
-
-
-@app.route('/', methods=['POST'])
-def save_url():
-    link = request.form['url']
-    uuid = get_short_code()
-
-    cur.execute("INSERT INTO urls(uuid,link) VALUES(?,?)", (uuid, link))
-    conn.commit()
-
-    url = "http://{}/{}".format(request.host, uuid)
-    response = "<a href='{}'>{}</a>".format(url, url)
-    return render_template_string(response)
-
-
-@app.route('/<uuid>', methods=['GET'])
-def show_table(uuid):
-    cur.execute("SELECT link FROM urls WHERE uuid='{}';".format(uuid))
-    links = cur.fetchall()
-    if len(links) > 0:
-        page_url = links[0][0]
-    else:
-        page_url = ''
-        abort(404)
-
+def get_rating_data(page_url):
     content = requests.get(page_url).content
 
     soup = BeautifulSoup(content, 'html.parser')
@@ -83,7 +53,7 @@ def show_table(uuid):
             data_row.append(' '.join(list(map(
                 lambda x: BeautifulSoup(str(x), 'html.parser').get_text(),
                 cell.contents))).strip()
-            )
+                            )
         if len(data_row[0]) == 0:
             continue
         for i in range(0, len(data_row)):
@@ -91,14 +61,98 @@ def show_table(uuid):
                 data_row[i] = '-'
         data_rows.append(data_row)
 
+    return data_rows
+
+
+def filter_rating(rating_data, region=''):
+    rating_data = [row for row in rating_data if region != '' and row[2] == region]
+    for place in range(0, len(rating_data)):
+        rating_data[place][0] = place + 1
+    return rating_data
+
+
+def get_header(rating_data):
+    columns = [
+        'Place',
+        'User',
+        'Region',
+        'Total',
+        'Penalty',
+    ]
+
+    letters = []
+    for letter_num in range(0, len(rating_data[0]) - len(columns)):
+        letters.append(chr(letter_num + ord('A')))
+    columns = columns[:3] + letters + columns[3:]
+    return columns
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def show_form():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def save_url():
+    link = request.form['url']
+    uuid = get_short_code()
+
+    cur.execute("INSERT INTO urls(uuid,link) VALUES(?,?)", (uuid, link))
+    conn.commit()
+
+    csv_url = "http://{}/{}".format(request.host, uuid)
+    html_url = "http://{}/{}.html".format(request.host, uuid)
+    return render_template('urls.html', csv_url=csv_url, html_url=html_url)
+
+
+@app.route('/<uuid>', methods=['GET'])
+def show_csv(uuid):
+    cur.execute("SELECT link FROM urls WHERE uuid='{}';".format(uuid))
+    links = cur.fetchall()
+    if len(links) > 0:
+        page_url = links[0][0]
+    else:
+        page_url = ''
+        abort(404)
+
+    rating_data = get_rating_data(page_url)
+    header = get_header(rating_data)
+
     out = StringIO()
     wr = csv.writer(out)
-    for row in data_rows:
+
+    wr.writerow(header)
+    for row in rating_data:
         wr.writerow(row)
 
     response = make_response(out.getvalue())
     response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Content-type', 'text/csv')
+    response.headers.add('Content-disposition', 'attachment;filename={}.csv'.format(uuid))
     return response
+
+
+@app.route('/<uuid>.html', methods=['GET'])
+def show_table(uuid):
+    cur.execute("SELECT link FROM urls WHERE uuid='{}';".format(uuid))
+    links = cur.fetchall()
+    if len(links) > 0:
+        page_url = links[0][0]
+    else:
+        page_url = ''
+        abort(404)
+
+    rating_data = get_rating_data(page_url)
+    columns = get_header(rating_data)
+
+    region = link = request.args.get('region')
+    if region:
+        rating_data = filter_rating(rating_data, region)
+
+    return render_template('table.html', data=rating_data, columns=columns)
 
 
 if __name__ == "__main__":
